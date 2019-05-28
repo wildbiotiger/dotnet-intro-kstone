e4b83772cbc2a8637b2c8f88a23695affb560017
karlastone4 <46212098+karlastone4@users.noreply.github.com>
2019-05-28 11:26:48 -0500

    Update MainViewModel.cs
    Remove extra white space
    Add comments to explain methods
    Change collection to singular name
    Remove unused usings

--- Pirates/Pirates/ViewModel/MainViewModel.cs
+++ Pirates/Pirates/ViewModel/MainViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Collections.ObjectModel;
-using System.Windows.Controls;
 
 namespace Pirates
 {
@@ -12,31 +6,25 @@ namespace Pirates
     {
         public MainViewModel()
         {
-
-            Pirates = new ObservableCollection<PirateNames>
+            //creates observable collection
+            Pirates = new ObservableCollection<PirateName>
             {
-
-
-                new PirateNames {Name = "Davy Jones"},
-                new PirateNames {Name = "Black Beard"},
-
+                new PirateName {Name = "Davy Jones"},
+                new PirateName {Name = "Black Beard"},
             };
         }
-
-
-
         public static MainViewModel DataContext { get; set; }
         public ObservableCollection<PirateNames> Pirates { get; set; }
-
+        
+        //Method to add pirate names to listbox
         public void AddPirates(string newPirate)
         {
-            Pirates.Add(new PirateNames { Name = newPirate });
+            Pirates.Add(new PirateName { Name = newPirate });
         }
-
-        public void RemovePirates(int losePirates)
+        //Method to remove pirate names from listbox
+        public void RemovePirate(int losePirate)
         {
-            Pirates.RemoveAt(losePirates);
+            Pirates.RemoveAt(losePirate);
         }
     }
-
 }
