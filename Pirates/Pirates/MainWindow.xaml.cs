0b684af4b0ee5a0aa3da53ab3334ad272f47db7c
karlastone4 <46212098+karlastone4@users.noreply.github.com>
2019-05-28 11:05:51 -0500

    Update MainWindow.xaml.cs
    Remove extra whitespace
    Remove unused usings
    Add comments
    Make names signular

--- Pirates/Pirates/MainWindow.xaml.cs
+++ Pirates/Pirates/MainWindow.xaml.cs
@@ -1,99 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Pirates
+namespace Pirate
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-
-
     public partial class MainWindow : Window
-
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-
-
         //adding the textbox info to pirates collection by clicking add button
         private void Add_Click(object sender, RoutedEventArgs e)
         {
             ((MainViewModel)DataContext).AddPirates(TextBox.Text);
         }
-
-        //clearing textbox by hitting the enter key
-        private void TextBox_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                TextBox.Clear();
-            }
-        }
-        //adding the textbox info to pirates collection by hitting enter
-
-
         //removing highlighted pirate from listbox and collection by clicking remove button
         private void Remove_Click(object sender, RoutedEventArgs e)
         {
             ((MainViewModel)DataContext).RemovePirates(ListBox.SelectedIndex);
         }
-
+        //removing highlighted pirate from listbox and collection by hitting delete on keyboard
         private void ListBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Delete)
                 ((MainViewModel)DataContext).RemovePirates(ListBox.SelectedIndex);
         }
-
+        //Add pirates to listbox by hitting enter on keyboard
         private void EnterAdd(object sender, KeyEventArgs e)
         {
-
             if (e.Key == Key.Enter)
                 ((MainViewModel)DataContext).AddPirates(TextBox.Text);
-
         }
-
+        //clearing textbox by hitting enter on keyboard
         private void EnterClear(object sender, KeyEventArgs e)
         {
-
             if (e.Key == Key.Enter)
             {
                 TextBox.Clear();
             }
-
         }
-
-
-
-
-
-
-
-
-        //removing highlighted pirate from listbox and collectin by hitting delete key
-
-
-
     }
-
 }
-
-
-
-
-
-
-
