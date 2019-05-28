05f99af370841d8bdc3ba6dd9e79da23e0bf08e8
karlastone4 <46212098+karlastone4@users.noreply.github.com>
2019-05-28 10:16:29 -0500

    Initial commit to data-binding

---
+++ Pirates/Pirates/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace Pirates
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+
+            Pirates = new ObservableCollection<PirateNames>
+            {
+
+
+                new PirateNames {Name = "Davy Jones"},
+                new PirateNames {Name = "Black Beard"},
+
+            };
+        }
+
+
+
+        public static MainViewModel DataContext { get; set; }
+        public ObservableCollection<PirateNames> Pirates { get; set; }
+
+        public void AddPirates(string newPirate)
+        {
+            Pirates.Add(new PirateNames { Name = newPirate });
+        }
+
+        public void RemovePirates(int losePirates)
+        {
+            Pirates.RemoveAt(losePirates);
+        }
+    }
+
+}
---
+++ Pirates/Pirates/ViewModel/PirateNames.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirates
+{
+    public class PirateNames
+    {
+        public string Name { get; set; }
+
+
+    }
+}
